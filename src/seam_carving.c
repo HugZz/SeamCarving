ce81b54df81ab45e9149b0a78b6908cdae856e2d
Hugues de Valon <hugues.devalon@gmail.com>
2016-08-07 15:24:28 +0200

    Add free functions on failure
    The structures used by a function should be freeed when it fails before
    returning the error value. If the function returns an allocated
    structure, it also need to be freeed because the receiving function
    won't be able to.

--- src/seam_carving.c
+++ src/seam_carving.c
@@ -44,38 +44,66 @@ uint8_t **seam_carving(uint8_t **image, uint32_t columns_to_delete,
 		energy = gradienty(new_image, nl, nc);
 		if (energy == NULL) {
 			ERROR("calculating energy failed.\n");
+			free_image((void **) new_image);
 			return NULL;
 		}
 		father_column = (uint32_t **) alloc_image(nl, nc, INT);
 		if (father_column == NULL) {
 			ERROR("calculating energy failed.\n");
+			free_image((void **) new_image);
+			free_image((void **) energy);
 			return NULL;
 		}
 		final_cost = calloc(nc, sizeof(uint32_t));
 		if (final_cost == NULL) {
 			perror("Allocation of final_cost failed.");
+			free_image((void **) father_column);
+			free_image((void **) new_image);
+			free_image((void **) energy);
 			return NULL;
 		}
 		if (cost(father_column, final_cost, energy, nl, nc) < 0) {
 			ERROR("failed calculating cost.\n");
+			free(final_cost);
+			free_image((void **) father_column);
+			free_image((void **) new_image);
+			free_image((void **) energy);
 			return NULL;
 		}
 		best_path = calloc(nl, sizeof(unsigned int));
 		if (best_path == NULL) {
 			perror("Allocation of best_path failed.");
+			free(final_cost);
+			free_image((void **) father_column);
+			free_image((void **) new_image);
+			free_image((void **) energy);
 			return NULL;
 		}
 		if (optimal_path(best_path, father_column, final_cost, nl, nc)
 				< 0) {
 			ERROR("failed finding the optimal path.\n");
+			free(final_cost);
+			free(best_path);
+			free_image((void **) father_column);
+			free_image((void **) new_image);
+			free_image((void **) energy);
 			return NULL;
 		}
 		if (delete_column(new_image, best_path, nl,nc) < 0) {
 			ERROR("failed deleting columns.\n");
+			free(final_cost);
+			free(best_path);
+			free_image((void **) father_column);
+			free_image((void **) new_image);
+			free_image((void **) energy);
 			return NULL;
 		}
 		nc--;
 	}
+	free(final_cost);
+	free(best_path);
+	free_image((void **) father_column);
+	free_image((void **) energy);
 
 	return new_image;
 }
